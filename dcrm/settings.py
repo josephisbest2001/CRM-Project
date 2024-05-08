4f2cb7ebf0317552731612e19ad16e5c46f55c16
josephisbest2001 <josephisbest15102001@gmail.com>
2024-05-08 13:38:16 +0530

    Inserted Successfully

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'elderco',
         'USER': 'root',
-        'PASSWORD': 'password123',
+        'PASSWORD': 'Joseph@2001',
         'HOST': 'localhost',
         'PORT': '3306',
     }
